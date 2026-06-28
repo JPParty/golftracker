8edb19324d2fc52481554051ac1c856f119024a2
JPParty <jacobwpool@gmail.com>
2026-06-28 01:10:55 -0500

    Update index.js
    fixed leaderboard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const CACHE_SECONDS = 301;
-const LPGA_URL = "https://www.lpga.com/leaderboard";
+const LPGA_LEADERBOARD_URL = "https://www.lpga.com/tournaments/kpmgwomenspgachampionship/leaderboard";
 
 let memoryCache = {
   expiresAt: 0,
